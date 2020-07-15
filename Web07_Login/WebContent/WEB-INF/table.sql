78586caf8451d5a3678bda3ff35a2ece3bd5b5c6
parkjye <parkjye44@gmail.com>
2020-07-15 12:11:26 +0900

    <문서>: <table.sql>
    board_file 생성을 위해 table.sql문서에 쿼리문 추가합니다.

--- Web07_Login/WebContent/WEB-INF/table.sql
+++ Web07_Login/WebContent/WEB-INF/table.sql
@@ -4,4 +4,16 @@ create table users(
 	email varchar2(100),
 	profile varchar2(100), --프로필 이미지 경로를 저장할 컬럼
 	regdate date
-)
+);
+
+create table board_file(
+	nun number primary key,
+	writer varchar2(100) not null,
+	title varchar2(100) not null,
+	orgFileName varchar2(100) not null, --원본 파일명
+	saveFileName varchar2(100) not null, --서버에 실제로 저장된 파일명
+	fileSize number not null,
+	regdate date
+);
+
+create sequence board_file_seq;
